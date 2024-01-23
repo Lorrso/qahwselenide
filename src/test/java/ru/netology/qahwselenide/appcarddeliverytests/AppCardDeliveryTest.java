18b84488561d151a98421fc5263ffd65928ae125
AnnaShev <anreich0@gmail.com>
2024-01-23 15:52:38 +0300

    gradle.yml, AppCardDeliveryTest.java was modified

--- src/test/java/ru/netology/qahwselenide/appcarddeliverytests/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/qahwselenide/appcarddeliverytests/AppCardDeliveryTest.java
@@ -1,14 +1,15 @@
 package ru.netology.qahwselenide.appcarddeliverytests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -16,18 +17,20 @@ public class AppCardDeliveryTest {
     @Test
     void shouldSendFormPositivePath() {
         LocalDate date = LocalDate.now();
-        date = date.plusDays(5);
+        date = date.plusDays(4);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String formDate = date.format(formatter);
 
         open("http://localhost:9999");
-        SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue("Иркутск");
-        form.$("[data-test-id=date] input").doubleClick().setValue(formDate);
-        form.$("[data-test-id=name] input").setValue("Шевченко Анна");
-        form.$("[data-test-id=phone] input").setValue("+79960000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$(".button").click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=city] input").setValue("Иркутск");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(formDate);
+        $("[data-test-id=name] input").setValue("Шевченко Анна");
+        $("[data-test-id=phone] input").setValue("+79960000000");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + formDate));
     }
 }
